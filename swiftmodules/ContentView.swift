47371c0b095ebab529d6ce463b0e376c0bd5e8b1
Michael <michq4t@gmail.com>
2020-07-17 11:10:44 -0500

    set git

---
+++ swiftmodules/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftmodules
+//
+//  Created by michael on 7/17/20.
+//  Copyright © 2020 michael. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
